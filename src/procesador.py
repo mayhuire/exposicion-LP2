4658fa35ebadc1234ab88e54bf3e0c67ae5be0c6
PomaJosue <jpoma1573@gmail.com>
2026-06-29 10:56:27 -0500

    IDENTAR METODOS DE CLASE Y REBUSTECER BUSQUEDA DE INFLACION

--- src/procesador.py
+++ src/procesador.py
@@ -16,33 +16,53 @@ class ProcesadorEconomico:
         self.df = pd.concat(lista_df, ignore_index=True)
         return self.df
     
-# LIMPIEZA Y REGEX
 
-def limpiar_y_transformar(self):
+    # LIMPIEZA Y REGEX
 
-    df = self.df.copy()
+    def limpiar_y_transformar(self):
+        df = self.df.copy()
 
-    df["anio"] = df["fecha_original"].astype(str).str.extract(r"(\d{4})")
-    df["anio"] = df["anio"].astype(int)
+        df["anio"] = df["fecha_original"].astype(str).str.extract(r"(\d{4})")
+        df["anio"] = df["anio"].astype(int)
 
-    df["valor"] = pd.to_numeric(df["valor"], errors="coerce")
-    df = df.dropna(subset=["valor"])
+        df["valor"] = pd.to_numeric(df["valor"], errors="coerce")
+        df = df.dropna(subset=["valor"])
 
-    df = df.sort_values(by=["pais", "indicador", "anio"])
+        df = df.sort_values(by=["pais", "indicador", "anio"])
 
-    self.df = df
-    return df
+        self.df = df
+        return df
 
-#ESTADÍSTICAS Y EXPORTACIÓN 
+    # ESTADÍSTICAS Y EXPORTACIÓN
 
-def calcular_estadisticas(self):
+    def calcular_estadisticas(self):
+        df = self.df
 
-    return self.df.groupby(["pais", "indicador"])["valor"].agg([
-        "mean", "max", "min"
-    ]).reset_index()
+        resumen = df.groupby(["pais", "indicador"])["valor"].agg([
+            "mean",
+            "max",
+            "min"
+        ]).reset_index()
 
+        # CRECIMIENTO PIB
+        pib = df[df["indicador"].str.contains("PIB", case=False, na=False)].copy()
+        pib["crecimiento"] = pib.groupby("pais")["valor"].pct_change() * 100
 
-def exportar_limpio(self, ruta="data/procesado/datos_limpios.csv"):
+        # CATEGORIZACIÓN DE INFLACIÓN
+        def categorizar(x):
+            if x < 3:
+                return "Baja"
+            elif x < 10:
+                return "Moderada"
+            else:
+                return "Alta"
 
-    self.df.to_csv(ruta, index=False)
-    return ruta
+        mask = df["indicador"].str.contains("Inflaci[oó]n", case=False, na=False)
+        df.loc[mask, "categoria_inflacion"] = df.loc[mask, "valor"].apply(categorizar)
+
+        self.df = df
+        return resumen
+
+    def exportar_limpio(self, ruta="data/procesado/datos_limpios.csv"):
+        self.df.to_csv(ruta, index=False)
+        return ruta
